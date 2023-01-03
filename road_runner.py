b040172bbcc91afdff8062bf6293a205464d94d3
SymonBezerra <simaobezerratranslate@gmail.com>
2023-01-03 15:05:40 -0300

    desenhando o quadrado branco

--- road_runner.py
+++ road_runner.py
@@ -1,6 +1,22 @@
 import pygame
+# inicializando a tela do nosso jogo
+screen = pygame.display.set_mode([800,600])
 
 if __name__ == "__main__":
-    # aqui definimos a função principal a ser executada
-    # pelo interpretador Python
+    
     pygame.init()
+
+    # criando a variável que gerenciará o nosso game loop
+    running = True
+
+    while running:
+        # recebendo a entrada de comandos
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.draw.rect(screen, (255,255,255), rect=(400, 300, 50, 50))
+
+        pygame.display.flip()
+    
+    pygame.quit()
