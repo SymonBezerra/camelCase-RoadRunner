b8593a080f1d11c3535420b32991357fa567faff
SymonBezerra <simaobezerratranslate@gmail.com>
2023-01-03 15:38:14 -0300

    -

--- road_runner.py
+++ road_runner.py
@@ -1,31 +1,33 @@
 import pygame
 # setas do teclado + tecla pressionada
 from pygame.locals import K_UP, K_DOWN, K_RIGHT, K_LEFT, KEYDOWN
-# inicializando a tela do nosso jogo
 screen = pygame.display.set_mode([800,600])
 
 if __name__ == "__main__":
     
     pygame.init()
 
-    # criando a variável que gerenciará o nosso game loop
     running = True
 
     while running:
-        # recebendo a entrada de comandos
         rect_x = 400
         rect_y = 300
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            # em caso de tecla pressionada
             elif event.type == KEYDOWN:
+                # seta para cima
                 if event.key == K_UP:
                     rect_y -= 5 
+                # seta para baixo
                 elif event.key == K_DOWN:
                     rect_y += 5
+                # seta para esquerda
                 elif event.key == K_LEFT:
                     rect_x -= 5
+                # seta para direita
                 elif event.key == K_RIGHT:
                     rect_x += 5
 
